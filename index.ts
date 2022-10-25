65af16b0f3fcb03a9099465ab4d9987083697817
Fabien Zucchet <fabien.zucchet@viarezo.fr>
2022-10-25 15:04:43 +0200

    feat(index): use nlp to compute similarity

--- index.ts
+++ index.ts
@@ -1,10 +1,28 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
+const WORD2VEC_API_URL = "http://nlp.polytechnique.fr/similarityscore";
+const WORD = "chat";
+
 async function handler(_req: Request): Promise<Response> {
     const payload = await _req.formData();
     const word = payload?.get("text");
-    console.log(word);
-  return new Response("Mangez vos darons Kévin et Melon");
+
+    const response = await fetch(WORD2VEC_API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            lang: "fr",
+            sim1: word,
+            sim2: WORD,
+            type: "General Word2Vec",
+        }),
+    });
+
+    console.log(await response.json());
+
+  return new Response("Vous avez saisi : " + word);
 }
 
 serve(handler);
