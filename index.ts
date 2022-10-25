1f8a232b0957cb77cab58f0aaebcd8988931ad3a
Fabien Zucchet <fabien.zucchet@viarezo.fr>
2022-10-25 14:18:43 +0200

    feat(index): fetch the word in the slash command

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-  return new Response("Hello World");
+    const payload = await _req.formData();
+    const word = payload?.get("text");
+    console.log(word);
+  return new Response("Mangez vos darons Kévin et Melon");
 }
 
 serve(handler);
